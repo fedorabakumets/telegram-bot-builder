41815f125a8a9ebca0e16a99917225381cf27885
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-02-11 05:42:15 +0300

    Добавлен w-full класс к контейнеру BotTerminal для фиксированной ширины

--- client/src/components/editor/BotTerminal.tsx
+++ client/src/components/editor/BotTerminal.tsx
@@ -44,7 +44,7 @@ export function BotTerminal({ projectId, tokenId, isBotRunning }: BotTerminalPro
 
   return (
     <>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 w-full">
         <div className="flex items-center gap-2">
           <Button
             onClick={() => setTerminalVisible(!terminalVisible)}
