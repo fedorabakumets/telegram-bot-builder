41100ebd5c61aa39d75ca983e57e54db5c5073db
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-02-11 06:43:34 +0300

    Теперь при редактировании токена отображается полный токен вместо маскированного

--- client/src/components/editor/TokenDisplayEdit.tsx
+++ client/src/components/editor/TokenDisplayEdit.tsx
@@ -78,7 +78,7 @@ export function TokenDisplayEdit({ token, tokenId, projectId, onTokenUpdate }: T
    */
   const handleDoubleClick = () => {
     setIsEditing(true);
-    setTokenValue(token);
+    setTokenValue(token); // Отображаем полный токен при редактировании
     setValidationError(null);
   };
 
