b4881cbb5e16fc9e2aa677ef40d8cea987d3c754
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-02-27 15:13:51 +0300

    Добавлен index.ts для groups/members/

---
+++ server/routes/botIntegration/groups/members/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview Экспорт хендлеров управления участниками группы
+ *
+ * @module botIntegration/groups/members
+ */
+
+export { restrictMemberHandler } from "./restrictMember.handler";
