46eca2d52c8fdfe424c5abc2638cf8982d5e0467
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-02-28 14:14:16 +0300

    fix: удаление неиспользуемого импорта multiselectcheck

--- client/lib/handle_user_input.ts
+++ client/lib/handle_user_input.ts
@@ -4,7 +4,6 @@ import { handleConditionalNavigationAndInputCollection } from './handleCondition
 import { handleNodeNavigationAndInputProcessing } from './handleNodeNavigationAndInputProcessing';
 import { generateReplyHideAfterClickHandler } from './Keyboard/generateReplyHideAfterClickHandler';
 import { mediafiles } from './MediaHandler/mediafiles';
-import { multiselectcheck } from './multiselectcheck';
 import { navigateaftersave } from './navigateaftersave';
 import { processUserInputWithValidationAndSave } from './processUserInputWithValidationAndSave';
 import { skip_button_target, skipDataCollection, skipDataCollectionnavigate } from './skipDataCollection';
