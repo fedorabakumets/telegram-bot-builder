6343b3c0318e1ad7c1a98eaae3dfa3e97bcc58d5
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-02-28 13:19:55 +0300

    refactor: выделение модуля transitions для генерации переходов между узлами

---
+++ client/lib/bot-generator/transitions/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Бочка для модуля transitions
+ * 
+ * Экспортирует все функции генерации переходов между узлами.
+ * 
+ * @module bot-generator/transitions
+ */
+
+export { generateConditionalBranches } from './generate-conditional-branches';
