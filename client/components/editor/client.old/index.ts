4d73a30fa34447d9cf25d940bcf3f8f5b9fc96bc
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-03-07 12:08:14 +0300

    refactor: удалены старые файлы компонентов Telegram Client API

--- client/components/editor/client.old/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview Экспорт компонентов Telegram Client API
- *
- * @module telegram-client
- */
-
-export { TelegramClientConfig } from './TelegramClientConfig';
-export { GroupMembersClientPanel } from './GroupMembersClientPanel';
-export { TEST_CONFIG } from './test-config';
