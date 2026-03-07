bc6fbcf234eeefbd39f88fe7467c4dacae3e6344
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-03-07 14:39:38 +0300

    fix: исправлена структура JSX в properties-panel

--- client/components/editor/properties/components/main/properties-panel.tsx
+++ client/components/editor/properties/components/main/properties-panel.tsx
@@ -353,7 +353,6 @@ export function PropertiesPanel({
                       }}
                     />
                   )}
-                </div>
               </div>
             )}
           </div>
