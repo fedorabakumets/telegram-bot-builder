78776098cf681e8b0756a0701724720c74b88b6b
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-03-31 17:44:59 +0300

    фикс

--- client/components/editor/header/components/brand-section.tsx
+++ client/components/editor/header/components/brand-section.tsx
@@ -48,7 +48,7 @@ export function BrandSection({ projectName, botInfo, isVertical, isCompact, isMo
         isCompact={isCompact}
         isMobile={isMobile}
       />
-      <VersionBadge version="2.0.5" />
+      <VersionBadge version="2.0.9" />
       {!isVertical && <GithubButton className="!p-0 !h-auto" />}
     </div>
   );
