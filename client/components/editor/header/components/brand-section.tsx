0fb61f5dcb4a1dab67cae3e1460155ba51b87004
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-04-09 10:56:03 +0300

    auto: agent changes

--- client/components/editor/header/components/brand-section.tsx
+++ client/components/editor/header/components/brand-section.tsx
@@ -48,7 +48,7 @@ export function BrandSection({ projectName, botInfo, isVertical, isCompact, isMo
         isCompact={isCompact}
         isMobile={isMobile}
       />
-      <VersionBadge version="2.1.2" />
+      <VersionBadge version="2.1.3" />
       {!isVertical && <GithubButton className="!p-0 !h-auto" />}
     </div>
   );
