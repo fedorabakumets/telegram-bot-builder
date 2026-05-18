ca9a3df4add289cc2d871b91521c63f6f9ee5c14
fedorabakumets <81466782+fedorabakumets@users.noreply.github.com>
2026-05-18 15:52:41 +0300

    auto: agent changes

--- server/routes/botIntegration/user/index.ts
+++ server/routes/botIntegration/user/index.ts
@@ -7,4 +7,3 @@
 export { searchUserHandler } from "./searchUser.handler";
 export { resolveUserId } from "./utils/resolveUserId";
 export { getTelegramUser } from "./utils/getTelegramUser";
-export { searchLocalDatabase } from "./utils/searchLocalDatabase";
